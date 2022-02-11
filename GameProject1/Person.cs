a16c7538cac7eefa7f99a6eda79b06a4b4b1f484
Wes Steiner <wwes@ksu.edu>
2022-02-11 16:29:58 -0600

    Added person and coin classes, working on coin collection screen

---
+++ GameProject1/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using GameProject1.StateManagement;
+using GameProject1.Collisions;
+
+namespace GameProject1
+{
+    public class Person
+    {
+        Texture2D texture;
+        Color color;
+        Game game;
+
+        public Vector2 Position { get; set; }
+
+        public BoundingRectangle Bounds { get; set; }
+
+        public Person(Game game, Color color)
+        {
+            this.game = game;
+            this.color = color;
+        }
+
+        public void LoadContent()
+        {
+            texture = game.Content.Load<Texture2D>("OrangePerson");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (texture is null) throw new InvalidOperationException("Texture must be loaded to render");
+            spriteBatch.Draw(texture, Position, color);
+        }
+    }
+}
--- GameProject1/Screens/CoinJumpScreen.cs
+++ GameProject1/Screens/CoinJumpScreen.cs
@@ -7,11 +7,33 @@ using Microsoft.Xna.Framework.Input;
 using GameProject1.Screens;
 using GameProject1.StateManagement;
 using GameProject1.Collisions;
+using Microsoft.Xna.Framework.Content;
 
 namespace GameProject1.Screens
 {
     public class CoinJumpScreen : GameScreen
     {
+        private ContentManager _content;
+        private SpriteFont _gameFont;
+        private Person _player;
+        private Coin[] _coins;
 
+        public InputManager inputManager;
+
+        private readonly Random _random = new Random();
+        private readonly ScreenManager _screenManager;
+
+        private float _pauseAlpha;
+        private readonly InputAction _pauseAction;
+
+        public CoinJumpScreen(ScreenManager sM)
+        {
+            _pauseAction = new InputAction(
+                new[] { Buttons.Start, Buttons.Back },
+                new[] { Keys.Back, Keys.Escape },
+                true
+                );
+            _screenManager = sM;
+        }
     }
 }
